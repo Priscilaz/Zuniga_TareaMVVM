8d000cfe7f8cd35ee58fe8bd1710c447b57855aa
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-06-15 23:35:19 -0500

    Cambios en AllNotesPage y NotesViewModel

--- PriscilaZunigaApuntesMaui/ViewModels/PZNotesViewModel.cs
+++ PriscilaZunigaApuntesMaui/ViewModels/PZNotesViewModel.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using PriscilaZunigaApuntesMaui.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PriscilaZunigaApuntesMaui.ViewModels
 {
-    internal class PZNotesViewModel
+    internal class PZNotesViewModel : IQueryAttributable
     {
+        public ObservableCollection<ViewModels.PZNoteViewModel> PZAllNotes { get; }
+        public ICommand? PZNewCommand { get; }
+        public ICommand? PZSelectNoteCommand { get; }
+
+        public PZNotesViewModel()
+        {
+            PZAllNotes = new ObservableCollection<ViewModels.PZNoteViewModel>(Models.PZNote.LoadAllPZ().Select(n => new PZNoteViewModel(n)));
+            PZNewCommand = new AsyncRelayCommand(PZNewNoteAsync);
+            PZSelectNoteCommand = new AsyncRelayCommand<ViewModels.PZNoteViewModel>(PZSelectNoteAsync);
+        }
+
+        private async Task PZNewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.PZNotePage));
+        }
+
+        private async Task PZSelectNoteAsync(ViewModels.PZNoteViewModel pznote)
+        {
+            if (pznote != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.PZNotePage)}?load={pznote.PZIdentifier}");
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                PZNoteViewModel matchedNote = PZAllNotes.Where((n) => n.PZIdentifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    PZAllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                PZNoteViewModel matchedNote = PZAllNotes.Where((n) => n.PZIdentifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                    matchedNote.PZReload();
+
+                // If note isn't found, it's new; add it.
+                else
+                    PZAllNotes.Add(new PZNoteViewModel(PZNote.LoadPZ(noteId)));
+            }
+        }
     }
 }
--- PriscilaZunigaApuntesMaui/Views/PZAllNotesPage.xaml.cs
+++ PriscilaZunigaApuntesMaui/Views/PZAllNotesPage.xaml.cs
@@ -7,31 +7,31 @@ public partial class PZAllNotesPage : ContentPage
     {
         InitializeComponent();
 
-        BindingContext = new Models.PZAllNotes();
+        //BindingContext = new Models.PZAllNotes();
     }
 
-    protected override void OnAppearing()
-    {
-        ((Models.PZAllNotes)BindingContext).LoadNotes();
-    }
+    //protected override void OnAppearing()
+    //{
+    //    ((Models.PZAllNotes)BindingContext).LoadNotes();
+    //}
 
-    private async void Add_Clicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync(nameof(PZNotePage));
-    }
+    //private async void Add_Clicked(object sender, EventArgs e)
+    //{
+    //    await Shell.Current.GoToAsync(nameof(PZNotePage));
+    //}
 
-    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (e.CurrentSelection.Count != 0)
-        {
-            // Get the note model
-            var note = (Models.PZNote)e.CurrentSelection[0];
+    //private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //{
+    //    if (e.CurrentSelection.Count != 0)
+    //    {
+    //        // Get the note model
+    //        var note = (Models.PZNote)e.CurrentSelection[0];
 
-            // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
-            await Shell.Current.GoToAsync($"{nameof(PZNotePage)}?{nameof(PZNotePage.ItemId)}={note.Filename}");
+    //        // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
+    //        await Shell.Current.GoToAsync($"{nameof(PZNotePage)}?{nameof(PZNotePage.ItemId)}={note.Filename}");
 
-            // Unselect the UI
-            notesCollection.SelectedItem = null;
-        }
-    }
+    //        // Unselect the UI
+    //        notesCollection.SelectedItem = null;
+    //    }
+    //}
 }
