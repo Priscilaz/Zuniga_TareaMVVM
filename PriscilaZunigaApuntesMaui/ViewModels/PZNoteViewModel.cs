71b78f0372086c1ee876d7b6d11d52a663e9c0f0
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-06-15 22:52:06 -0500

    Actualizacion del NoteViewModel y Notepage.xaml. No hay errores

--- PriscilaZunigaApuntesMaui/ViewModels/PZNoteViewModel.cs
+++ PriscilaZunigaApuntesMaui/ViewModels/PZNoteViewModel.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace PriscilaZunigaApuntesMaui.ViewModels
+namespace PriscilaZunigaApuntesMaui.ViewModels;
+
+internal class PZNoteViewModel : ObservableObject, IQueryAttributable
 {
-    internal class PZNoteViewModel
+    private Models.PZNote? _pznote;
+
+    public string PZText
     {
+        get => _pznote.Text;
+        set
+        {
+            if (_pznote.Text != value)
+            {
+                _pznote.Text = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateTime PZDate => _pznote.Date;
+
+    public string PZIdentifier => _pznote.Filename;
+
+    public ICommand? PZSaveCommand { get; private set; }
+    public ICommand? PZDeleteCommand { get; private set; }
+
+    public PZNoteViewModel()
+    {
+        _pznote = new Models.PZNote();
+        PZSaveCommand = new AsyncRelayCommand(PZSave);
+        PZDeleteCommand = new AsyncRelayCommand(PZDelete);
+    }
+
+    public PZNoteViewModel(Models.PZNote pznote)
+    {
+        _pznote = pznote;
+        PZSaveCommand = new AsyncRelayCommand(PZSave);
+        PZDeleteCommand = new AsyncRelayCommand(PZDelete);
+    }
+
+    private async Task PZSave()
+    {
+        _pznote.Date = DateTime.Now;
+        _pznote.SavePZ();
+        await Shell.Current.GoToAsync($"..?saved={_pznote.Filename}");
+    }
+
+    private async Task PZDelete()
+    {
+        _pznote.DeletePZ();
+        await Shell.Current.GoToAsync($"..?deleted={_pznote.Filename}");
+    }
+
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("load"))
+        {
+            _pznote = Models.PZNote.LoadPZ(query["load"].ToString());
+            PZRefreshProperties();
+        }
+    }
+    public void PZReload()
+    {
+        _pznote = Models.PZNote.LoadPZ(_pznote.Filename);
+        PZRefreshProperties();
+    }
+
+    private void PZRefreshProperties()
+    {
+        OnPropertyChanged(nameof(Text));
+        OnPropertyChanged(nameof(Date));
     }
 }
--- PriscilaZunigaApuntesMaui/Views/PZNotePage.xaml.cs
+++ PriscilaZunigaApuntesMaui/Views/PZNotePage.xaml.cs
@@ -1,56 +1,59 @@
 namespace PriscilaZunigaApuntesMaui.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
+//[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class PZNotePage : ContentPage
 {
-   
-   
-    //string _fileName = Path.Combine(FileSystem.AppDataDirectory, "PZnotes.txt");
-
     public PZNotePage()
     {
         InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.PZnotes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.PZNote note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
+    //string _fileName = Path.Combine(FileSystem.AppDataDirectory, "PZnotes.txt");
 
-        await Shell.Current.GoToAsync("..");
-    }
+    //public PZNotePage()
+    //{
+    //    InitializeComponent();
 
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.PZNote note)
-        {
-            // Delete the file.
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
+    //    string appDataPath = FileSystem.AppDataDirectory;
+    //    string randomFileName = $"{Path.GetRandomFileName()}.PZnotes.txt";
 
-        await Shell.Current.GoToAsync("..");
-    }
-    private void LoadNote(string fileName)
-    {
-        Models.PZNote noteModel = new Models.PZNote();
-        noteModel.Filename = fileName;
+    //    LoadNote(Path.Combine(appDataPath, randomFileName));
+    //}
 
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
+    //private async void SaveButton_Clicked(object sender, EventArgs e)
+    //{
+    //    if (BindingContext is Models.PZNote note)
+    //        File.WriteAllText(note.Filename, TextEditor.Text);
 
-        BindingContext = noteModel;
-    }
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
+    //    await Shell.Current.GoToAsync("..");
+    //}
+
+    //private async void DeleteButton_Clicked(object sender, EventArgs e)
+    //{
+    //    if (BindingContext is Models.PZNote note)
+    //    {
+    //        // Delete the file.
+    //        if (File.Exists(note.Filename))
+    //            File.Delete(note.Filename);
+    //    }
+
+    //    await Shell.Current.GoToAsync("..");
+    //}
+    //private void LoadNote(string fileName)
+    //{
+    //    Models.PZNote noteModel = new Models.PZNote();
+    //    noteModel.Filename = fileName;
+
+    //    if (File.Exists(fileName))
+    //    {
+    //        noteModel.Date = File.GetCreationTime(fileName);
+    //        noteModel.Text = File.ReadAllText(fileName);
+    //    }
+
+    //    BindingContext = noteModel;
+    //}
+    //public string ItemId
+    //{
+    //    set { LoadNote(value); }
+    //}
 }
